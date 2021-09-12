8bd110fc5ca12f6f3676807d8eb4c8795dea6e3b
Chris <jetsboy2011@gmail.com>
2021-09-12 18:48:15 -0400

    first 1000 prime numbers

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PNumbers = 0;
+            for (int i = 2; PNumbers < 1000; i++)
+            {
+                int Counter = 0;
+                for (int j = 2; j < 1000; j++)
+                {
+                    if (i != j)
+                    {
+                        if (i % j == 0)
+                        {
+                            Counter = Counter + 1;
+                            
+                        }
+                        
+                    }
+                }
+                if (Counter == 0){Console.WriteLine(i); PNumbers++;}
+                
+            }
+            
+        }
+    }
+}
